1683e1ba4df3628144ce52915be474f879fef7f3
NhatMinh <nhatminhdang1996@gmail.com>
2021-08-22 09:38:06 +0700

    add category

---
+++ models/categoriesModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    }
+  }, 
+  {
+    timestamps: true
+  }
+)
+
+let Dataset = mongoose.models.category || mongoose.model('category', categorySchema)
+export default Dataset
